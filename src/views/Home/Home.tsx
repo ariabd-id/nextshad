e420c775dc7c02d86a01834268073a56224464c8
Ari Ab Ghufron <110314438+ariabd-id@users.noreply.github.com>
2024-09-28 15:35:50 +0700

    feat: add shadcn

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,7 +1,12 @@
+import { Button } from '@nextshad/components/ui/button'
 import React from 'react'
 
 function Home() {
-  return <div className='text-red-500'>Home</div>
+  return (
+    <div className='text-red-500'>
+      <Button variant='default'>Hello</Button>
+    </div>
+  )
 }
 
 export default Home
